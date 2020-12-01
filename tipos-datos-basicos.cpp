f5e5a5d3f1d5b5fc172f030114946c71058dfe79
Jhon Chavez <jhchg7@gmail.com>
2020-12-01 01:27:52 -0400

    Programación en C++ || Tipos de datos básicos en C++

---
+++ tipos-datos-basicos.cpp
@@ -0,0 +1,21 @@
+//Tipos de datos basicos en C++
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int entero = 15; //dato entero
+    float flotante = 10.45; //flotante
+    double mayor = 16.3456; //para numeros extensos
+    char letra = 'a'; //caracteres
+
+
+    // cout<<entero;
+    // cout<<flotante;
+    // cout<<mayor;
+    cout<<letra;
+
+
+    return 0;
+}
